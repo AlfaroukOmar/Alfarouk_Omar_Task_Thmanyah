ee6862e16b16f0a66352e8234a13f6c1dc2317d9
alfarouk omar <elfarouk17@gmail.com>
2026-05-10 22:59:48 +0300

    Define API interfaces and DTOs for home feed and search

---
+++ data/remote/src/main/java/com/thmanyah/data/remote/dto/HomePageDto.kt
@@ -0,0 +1,37 @@
+
+package com.thmanyah.data.remote.dto
+
+import kotlinx.serialization.InternalSerializationApi
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.JsonArray
+import kotlinx.serialization.json.JsonElement
+
+
+@InternalSerializationApi
+data class HomePageDto(
+    val sections: List<SectionDto> = emptyList(),
+    val pagination: PaginationDto? = null,
+)
+
+@InternalSerializationApi
+@Serializable
+data class PaginationDto(
+    val next_page: String? = null,
+    val total_pages: Int? = null,
+)
+
+@InternalSerializationApi
+@Serializable
+data class SectionDto(
+    val name: String? = null,
+    val type: String? = null,
+    val content_type: String? = null,
+    val order: JsonElement? = null,
+    val content: JsonArray? = null,
+)
+
+@InternalSerializationApi
+@Serializable
+data class SearchResponseDto(
+    val sections: List<SectionDto> = emptyList(),
+)
